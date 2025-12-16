94dfd941307a0ba9476ca46275487eb1be893761
Des Whelan <wheland6@tcd.ie>
2025-12-16 10:38:06 +0000

    print details of overnight stock price changes of +- 5 percent

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+import config
+import requests
+
+COMPANY_NAME = "Chubb Ltd"
+# Overnight percentage change in stock price which should trigger alert
+DAILY_FLUCTUATION_THRESHOLD = 5
+SYMBOL = "CB"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+alpha_vantage_params ={
+    "function": "TIME_SERIES_DAILY",
+    "symbol": SYMBOL,
+    "apikey": config.ALPHA_VANTAGE_API_KEY
+}
+
+response = requests.get("https://www.alphavantage.co/query", alpha_vantage_params)
+response.raise_for_status()
+
+daily_stock_data_dict = response.json()["Time Series (Daily)"]
+daily_stock_data_list = [daily_stock_data_dict[daily_stock_data] for daily_stock_data in daily_stock_data_dict]
+
+for i, daily_stock_data in enumerate(daily_stock_data_dict):
+    daily_stock_data_list[i]["date"] = daily_stock_data
+
+for i, daily_stock_data in enumerate(daily_stock_data_list[:-1]):
+    opening_price = float(daily_stock_data["1. open"])
+    previous_closing_price = float(daily_stock_data_list[i + 1]['4. close'])
+    percentage_diff = ((opening_price - previous_closing_price)/opening_price) * 100
+
+    if abs(percentage_diff) >= DAILY_FLUCTUATION_THRESHOLD:
+        print(f"Stock moved by {percentage_diff}% from closing at ${previous_closing_price} on {daily_stock_data_list[i + 1]["date"]} to opening at ${opening_price} on {daily_stock_data["date"]}")
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
 
 #Optional: Format the SMS message like this: 
 """
